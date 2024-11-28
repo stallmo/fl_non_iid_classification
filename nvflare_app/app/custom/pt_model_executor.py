6927e79f03a851afd1c745c4c80b56efd2445633
stallmo <morris.stallmann@gmail.com>
2024-11-28 20:57:10 +0100

    add fl training executor and config

--- nvflare_app/app/custom/pt_model_executor.py
+++ nvflare_app/app/custom/pt_model_executor.py
@@ -1,6 +1,6 @@
-# NVFlare Custom Model Executor (local task execution
+# NVFlare Custom Model Executor (local task execution)
 import torch
-from nvflare.apis.dxo import from_shareable, DataKind
+from nvflare.apis.dxo import from_shareable, DataKind, DXO
 from nvflare.apis.executor import Executor
 from nvflare.apis.fl_context import FLContext
 from nvflare.apis.shareable import Shareable, make_reply
@@ -8,7 +8,8 @@ from nvflare.apis.signal import Signal
 from nvflare.app_common.app_constant import AppConstants
 from nvflare.apis.fl_constant import ReturnCode
 
-from src.utils import get_model, get_loss
+from src.utils import get_model, get_loss, get_data_centralized
+from src.pt_constants import PATH_TO_DATA_CENTRALIZED_DIR
 
 
 class PtModelExecutor(Executor):
@@ -42,11 +43,40 @@ class PtModelExecutor(Executor):
         # Optimizer
         self._optimizer = torch.optim.SGD(self._model.parameters(), lr=self._lr, momentum=0.9)
 
+        # get training and validation data (will be replaced with federated data in the future)
+        self._train_loader, self._val_loader, _ = get_data_centralized(dataset_name=model_name,
+                                                                       batch_size=self._batch_size,
+                                                                       root_dir=PATH_TO_DATA_CENTRALIZED_DIR
+                                                                       )
+
     def _train_model(self, fl_ctx: FLContext, weights: dict):
 
+        self.log_info(fl_ctx, f"Training the model for {self._epochs} epochs.")
         # Set model weights and set into training mode
         self._model.load_state_dict(weights)
         self._model.train()
+
+        # Execute training loop for the specified number of epochs
+        for epoch in range(self._epochs):
+            for i, data in enumerate(self._train_loader, 0):
+                inputs, labels = data
+                inputs, labels = inputs.to(self._device), labels.to(self._device)
+                self._optimizer.zero_grad()
+                outputs = self._model(inputs)
+                loss = self._criterion(outputs, labels)
+                loss.backward()
+                self._optimizer.step()
+
+    def share_weights(self, fl_ctx: FLContext) -> Shareable:
+        """
+        Share the model weights with the server.
+        """
+        weights = {k: v.cpu().detach().numpy() for k, v in self._model.state_dict().items()}
+        self.log_info(fl_ctx, f"Client {fl_ctx.get_identity_name()} shares updated weights with the server.")
+
+        outgoing_dxo = DXO(data_kind=DataKind.WEIGHTS, data=weights,
+                           meta={"num_steps_cur_round": len(self._train_loader)})
+        return outgoing_dxo.to_shareable()
 
     def execute(self, task_name: str, shareable: Shareable, fl_ctx: FLContext, abort_signal: Signal) -> Shareable:
 
@@ -63,9 +93,14 @@ class PtModelExecutor(Executor):
             # Convert weights to tensor and run training
             torch_weights = {k: torch.as_tensor(v) for k, v in incoming_dxo.data.items()}
             self._train_model(fl_ctx, torch_weights) # this updates the model weights
+
+            # share the updated weights
+            return self.share_weights(fl_ctx)
         elif task_name == self._submit_model_task_name:
             # Submit the model
-            self._submit_model(fl_ctx)
+            self.share_weights(fl_ctx)
+        elif task_name == 'get_weights':
+            return self.share_weights(fl_ctx)
         else:
             raise ValueError(f"Unknown task name: {task_name}")
 
